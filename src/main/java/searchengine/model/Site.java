9ea3db251ad7be14e1ad26a8dceac1d834f97aef
Andrii Osadchuk <Rudyst.1995@gmail.com>
2024-07-12 12:51:45 +0200

    Models are ready

--- src/main/java/searchengine/model/Site.java
+++ src/main/java/searchengine/model/Site.java
@@ -1,4 +1,39 @@
 package searchengine.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "Site")
 public class Site {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private SiteStatus status;
+
+    @Column(name = "status_time")
+    private LocalDateTime statusTime;
+
+    @Column(name = "last_error")
+    private String LastError;
+
+    @Column(name = "url")
+    private String url;
+
+    @Column(name = "name")
+    private String name;
+
+
 }
