06e36800cbfd88699a7b3dafcb343f5dd4d872f9
Andrii Osadchuk <Rudyst.1995@gmail.com>
2024-07-12 12:38:59 +0200

    first commit

---
+++ src/main/java/searchengine/model/Page.java
@@ -0,0 +1,37 @@
+package searchengine.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Page")
+public class Page{
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "site_id", nullable = false)
+    private Site site;
+
+    @Column(name = "path", columnDefinition = "TEXT")
+    private String path;
+
+    @Column(name = "code")
+    private int cade;
+
+    @Column(name = "content", columnDefinition = "MEDIUMTEXT")
+    private String content;
+
+
+}
---
+++ src/main/java/searchengine/model/Site.java
@@ -0,0 +1,4 @@
+package searchengine.model;
+
+public class Site {
+}
